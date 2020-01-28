bf55a528bf8d30b3b4f60479339b6618b28d50d4
Rick Belson <belsonrj@aol.com>
2020-01-28 01:48:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
-  number = 10 
-  while number > 0 
-    puts "#{number} SECONDS(S)!"
-    number -= 1
+    number = 10 
+    while number > 0 
+      puts "#{number} SECONDS(S)!"
+      number -= 1
   end
 end
 
